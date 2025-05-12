abf2017ef704ecf143c678072eb49d7b3e325e9a
Wallace Mariano <wallacewvinfo@gmail.com>
2025-05-12 17:36:55 -0300

    Exercicio 01 do dia 01.

---
+++ dia-01_variaveis.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+altura = input("Digite sua altura: ")
+
+print(f'Seu nome é {nome}, sua idade é de {idade} anos e você tem {altura} de altura.')
