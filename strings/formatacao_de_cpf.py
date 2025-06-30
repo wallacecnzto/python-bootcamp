cb12cd6f97755b3f0370cb3fbaa89eb45bcde035
Wallace Silva <wallacewvinfo@gmail.com>
2025-06-30 20:10:46 -0300

    Resolvedo exercicio de formatação de CPF.

---
+++ strings/formatacao_de_cpf.py
@@ -0,0 +1,10 @@
+cpf = input("Digite seu CPF (somente números): ").strip().casefold()
+qtd_digitos_cpf = 11
+
+while len(cpf) != qtd_digitos_cpf:
+    cpf = input("CPF inválido, digite novamente: ").strip().casefold()
+
+cpf_formatado = cpf[:3] + '.' + cpf[5:]
+cpf_formatado = cpf_formatado[:7] + '.' + cpf[6:]
+cpf_formatado = cpf_formatado[:11] + '-' + cpf[9:]
+print(cpf_formatado)
